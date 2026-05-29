2d943d81f239f8524081ede428177f3fe53fa37a
Leticia Takahashi <leticiasatiko@gmail.com>
2026-05-29 13:36:54 -0300

    contador

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 import Aleatorio from './components/Aleatorio'
-
+import Contador from './components/Contador'
 import MinMax from './components/MinMax'
 import CompUm, { Comp2, Comp3 } from './components/Multi'
 import Primeiro from './components/Primeiro'
+import Titulo from './components/Titulo'
+import Botao from './components/Botao'
 
 export default () => (
     <View style={style.App}>
-        <Aleatorio min={1} max={5} />
+        <Contador inicial={100} passo={10} />
 
-        {/* <MinMax min="3" max="10"/>
+        {/* <Botao /> */}
+        {/* <Titulo principal="Cadastro Produto" secundario="Tela de Cadastro" /> */}
+        {/* <Aleatorio min={1} max={5} />
+        <MinMax min="3" max="10"/>
         <MinMax min="1" max="94"/>
         <CompUm />
         <Comp2 />
